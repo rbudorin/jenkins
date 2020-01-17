b8cfeca5a18406b24469c510f73b3edcc35eb3c6
Roman Budorin <rbudorin@Romans-MacBook-Pro.local>
2020-01-17 15:49:19 +0300

    Initial commit.

---
+++ vars/telegram.groovy
@@ -0,0 +1,56 @@
+import hudson.tasks.test.AbstractTestResultAction
+import hudson.model.Actionable
+
+
+def sendTelegramStart(token, chatId) {
+    message = "<b>Build started</b>\n\n" +
+      "Project: <a href=\"$JOB_URL\">$JOB_NAME</a> \n" +
+      "Build number: <a href=\"$BUILD_URL\">$BUILD_DISPLAY_NAME</a> \n" +
+      "Branch: GIT_BRANCH"
+    
+    def encodedMessage = URLEncoder.encode(message, "UTF-8")
+    
+    response = httpRequest (consoleLogResponseBody: true,
+        contentType: 'APPLICATION_JSON',
+        httpMode: 'GET',
+        url: "https://api.telegram.org/bot$token/sendMessage?parse_mode=HTML&text=$encodedMessage&chat_id=$chatId&disable_web_page_preview=false",
+        validResponseCodes: '200')
+    
+    return response
+}
+
+@NonCPS
+def sendTelegramEnd(token, chatId) {
+    def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+    def testReport = ""
+
+    if (testResultAction != null) {
+        def total = testResultAction.getTotalCount()
+        def failed = testResultAction.getFailCount()
+        def skipped = testResultAction.getSkipCount()
+
+        testReport = testReport + ("Passed: " + (total - failed - skipped))
+        testReport = testReport + (", Failed: " + failed)
+        testReport = testReport + (", Skipped: " + skipped)
+    } else {
+        testReport = "No tests found."
+    }
+    
+    message = "<b>Build completed</b>\n\n" +
+        "Project: <a href=\"$JOB_URL\">$JOB_NAME</a> \n" +
+        "Status: $currentBuild.currentResult\n" +
+        "Build number: <a href=\"$BUILD_URL\">$BUILD_DISPLAY_NAME</a> \n" +
+        "Branch: GIT_BRANCH\n\n" +
+        "Test results\n" + 
+        "  $testReport"
+    
+    def encodedMessage = URLEncoder.encode(message, "UTF-8")
+    
+    response = httpRequest (consoleLogResponseBody: true,
+        contentType: 'APPLICATION_JSON',
+        httpMode: 'GET',
+        url: "https://api.telegram.org/bot$token/sendMessage?parse_mode=HTML&text=$encodedMessage&chat_id=$chatId&disable_web_page_preview=false",
+        validResponseCodes: '200')
+    
+    return response
+}
